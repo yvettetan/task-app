92dfda89601e1c07e37d5f741185bb07f2f129ea
Yvette Tan <yvettetan97@gmail.com>
2021-07-27 17:46:06 +0800

    Creates test for creating categories

---
+++ spec/system/create_categories_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'CreateCategories', driver: :selenium_chrome, js: true do
+  it 'creates and displays a category' do
+    # visit new category route
+    visit '/categories/new'
+    # fill in the form with needed information
+    fill_in 'Title', with: 'title'
+    fill_in 'Description', with: 'description'
+    # click a submit button
+    click_on 'Create Category'
+    # expect the page to have the content we submitted
+    expect(page).to have_content('title')
+    expect(page).to have_content('category')
+  end
+end
