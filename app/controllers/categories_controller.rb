5c7e49a9a7ff83fb8d3e921df0e67f261c99793e
Yvette Tan <yvettetan97@gmail.com>
2021-07-29 11:53:08 +0800

    Edits and updates category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,19 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
   def category_params
     params.require(:category).permit(:title, :description)
   end
