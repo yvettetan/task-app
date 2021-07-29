425cb8228b43cfd4c9c0e092a9766594263253ac
Yvette Tan <yvettetan97@gmail.com>
2021-07-29 11:35:29 +0800

    Creates test for updating categories

---
+++ spec/system/update_categories_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Updating Categories', type: :system, driver: :selenium_chrome, js: true do
+  it 'edits and updates a category' do
+    category = Category.create!(title: 'title', description: 'description')
+    # visit edit category route
+    visit edit_category_path(id: category.id)
+    # fill in the form with needed information
+    fill_in 'Title', with: 'title'
+    fill_in 'Description', with: 'description'
+    # click a submit button
+    click_on 'Update Category'
+    # expect the page to have the content we submitted
+    expect(page).to have_content('title')
+    expect(page).to have_content('description')
+  end
+end
