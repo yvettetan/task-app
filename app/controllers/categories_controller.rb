6b3c6aa77d56a6cd7e3c75f3e85adfbc9a5c9749
Yvette Tan <yvettetan97@gmail.com>
2021-07-29 12:25:49 +0800

    Validates presence of category title

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,10 +9,14 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-    if @category.save
-      redirect_to @category
-    else
-      render :new
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
     end
   end
 
