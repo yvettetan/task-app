813560b009f1b25bad22f876c04e1d30f3a6e137
Yvette Tan <yvettetan97@gmail.com>
2021-07-29 13:25:13 +0800

    Displays notice message for creating and updating categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -30,10 +30,14 @@ class CategoriesController < ApplicationController
 
   def update
     @category = Category.find(params[:id])
-    if @category.update(category_params)
-      redirect_to @category
-    else
-      render :edit
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
     end
   end
 
