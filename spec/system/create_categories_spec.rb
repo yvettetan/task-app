d39035462f2141c8f696ba63ae5bee869df0abc7
Yvette Tan <yvettetan97@gmail.com>
2021-07-29 12:24:58 +0800

    Creates test for invalid/missing category inputs

--- spec/system/create_categories_spec.rb
+++ spec/system/create_categories_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'CreateCategories', driver: :selenium_chrome, js: true do
+RSpec.describe 'Creating Categories', driver: :selenium_chrome, js: true do
   it 'creates and displays a category' do
     # visit new category route
     visit '/categories/new'
@@ -12,5 +12,19 @@ RSpec.describe 'CreateCategories', driver: :selenium_chrome, js: true do
     # expect the page to have the content we submitted
     expect(page).to have_content('title')
     expect(page).to have_content('description')
+  end
+
+  it 'is invalid without a title' do
+    visit '/categories/new'
+    fill_in 'Title', with: ''
+    click_on 'Create Category'
+    expect(page).to have_content("Title can't be blank")
+  end
+
+  it 'is invalid without a description' do
+    visit '/categories/new'
+    fill_in 'Description', with: ''
+    click_on 'Create Category'
+    expect(page).to have_content("Description can't be blank")
   end
 end
