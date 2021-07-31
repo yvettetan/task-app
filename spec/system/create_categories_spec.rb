b6f478cbe6d7908e27ebaf27506c244058ba3080
Yvette Tan <yvettetan97@gmail.com>
2021-08-01 00:13:57 +0800

    Removes sleep in create and update categories spec

--- spec/system/create_categories_spec.rb
+++ spec/system/create_categories_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe 'Creating Categories', driver: :selenium_chrome, js: true do
       within 'body' do
         expect(page).to have_content('Categories')
         expect(page).to have_link('New Category')
-        sleep(2)
       end
     end
   end
@@ -21,12 +20,10 @@ RSpec.describe 'Creating Categories', driver: :selenium_chrome, js: true do
         fill_in 'Title', with: 'title'
         fill_in 'Description', with: 'description'
         click_on 'Create Category'
-        sleep(3)
       end
       expect(page).to have_content('title')
       expect(page).to have_content('description')
       expect(page).to have_text('Category was successfully created')
-      sleep(3)
       category = Category.order('id').last
       expect(category.title).to eq('title')
       expect(category.description).to eq('description')
@@ -40,7 +37,6 @@ RSpec.describe 'Creating Categories', driver: :selenium_chrome, js: true do
       fill_in 'Description', with: 'description'
       click_on 'Create Category'
       expect(page).to have_text("Title can't be blank")
-      sleep(3)
     end
 
     it 'is invalid without a description' do
@@ -49,7 +45,6 @@ RSpec.describe 'Creating Categories', driver: :selenium_chrome, js: true do
       fill_in 'Description', with: ''
       click_on 'Create Category'
       expect(page).to have_text("Description can't be blank")
-      sleep(3)
     end
   end
 end
--- spec/system/update_categories_spec.rb
+++ spec/system/update_categories_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Updating Categories', type: :system, driver: :selenium_chrome, js: true do
   describe 'Updating a category' do
-    before { visit categories_path }
     it 'successfully updates a category' do
       category = Category.create!(title: 'Home', description: 'daily tasks at home')
       visit categories_path
@@ -12,15 +11,12 @@ RSpec.describe 'Updating Categories', type: :system, driver: :selenium_chrome, j
       within 'form' do
         fill_in 'Title', with: 'School Assignments'
         fill_in 'Description', with: 'daily assignments for school'
-        sleep(3)
         click_on 'Update Category'
       end
       expect(page).to have_current_path category_path(id: category.id)
       expect(page).to have_content('School Assignments')
       expect(page).to have_content('daily assignments for school')
       expect(page).to have_text('Category was successfully updated')
-      sleep(3)
-
       updated_category = Category.find(category.id)
       expect(updated_category.title).to eq('School Assignments')
       expect(updated_category.description).to eq('daily assignments for school')
