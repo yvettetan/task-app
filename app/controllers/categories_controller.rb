0073336d15aa282f23d0e00360d3afc9d0893d38
yvettetan <yvettetan97@gmail.com>
2021-08-02 17:56:25 +0800

    Refactors categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
+
   def index
     @categories = Category.all
   end
@@ -20,16 +22,11 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def show
-    @category = Category.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @category = Category.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @category = Category.find(params[:id])
     respond_to do |format|
       if @category.update(category_params)
         format.html { redirect_to @category, notice: 'Category was successfully updated.' }
@@ -42,12 +39,17 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
     respond_to do |format|
       format.html { redirect_to root_path, notice: 'Category was successfully deleted.' }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 
   def category_params
