9553df3124b228da37df4e4ace5ab7044694d321
yvettetan <yvettetan97@gmail.com>
2021-08-03 14:58:43 +0800

    Adds index new create show actions in tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,36 @@
 class TasksController < ApplicationController
+  before_action :set_category
+  before_action :set_task, only: %i[show]
+  def index
+    @tasks = @category.tasks
+  end
+
+  def new
+    @task = @category.tasks.build
+  end
+
+  def create
+    @task = @category.tasks.build(task_params)
+    if @category.save
+      redirect_to @task.category
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_task
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
 end
