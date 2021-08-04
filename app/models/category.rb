e0244f56e914fb20506de48fe5d7b06e28eda114
yvettetan <yvettetan97@gmail.com>
2021-08-04 12:08:21 +0800

    Allows deletion of category along with its associated tasks

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   validates :title, :description, presence: true
-  has_many :tasks
+  has_many :tasks, dependent: :destroy # ensures destroy action on a category deletes any associated tasks
 end
