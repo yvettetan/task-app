5b0903e269398220869111da46c71024ab1999b4
yvettetan <yvettetan97@gmail.com>
2021-08-04 16:00:31 +0800

    Updates test for creating tasks to include date

--- spec/system/create_tasks_spec.rb
+++ spec/system/create_tasks_spec.rb
@@ -5,18 +5,22 @@ RSpec.describe 'Creating Tasks', type: :system, driver: :selenium_chrome, js: tr
     @category = Category.create!(title: 'Title', description: 'description')
   end
   describe 'Creating a valid Task' do
-    it 'successfully creates a new task with name and description' do
+    it 'successfully creates a new task with name, description and due date' do
       visit category_path(@category.id)
       click_on 'Create Task'
       fill_in 'Name', with: 'Name'
       fill_in 'Description', with: 'Description'
+      page.find('#task_due_at').click
+      page.find('span', text: '30').click
       click_on 'Create Task'
       expect(page).to have_current_path category_path(@category.id)
       expect(page).to have_content('Name')
       expect(page).to have_content('Description')
+      expect(page).to have_content(Date.today.strftime('%b 30, %Y').to_s)
       task = Task.order('id').last
       expect(task.name).to eq('Name')
       expect(task.description).to eq('Description')
+      expect(task.due_at).not_to be_nil
     end
   end
 
@@ -34,6 +38,13 @@ RSpec.describe 'Creating Tasks', type: :system, driver: :selenium_chrome, js: tr
       fill_in 'Description', with: ''
       click_on 'Create Task'
       expect(page).to have_text("Description can't be blank")
+    end
+    it 'is invalid without a date' do
+      visit new_category_task_path(@category.id)
+      fill_in 'Name', with: 'Name'
+      fill_in 'Description', with: 'Description'
+      click_on 'Create Task'
+      expect(page).to have_text("Due at can't be blank")
     end
   end
 end
