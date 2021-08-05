e6e4ecbf9041c3d9d54472c8aaa2d872b5b5921d
yvettetan <yvettetan97@gmail.com>
2021-08-05 17:24:34 +0800

    Fixes error in creating tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_category, only: %i[show edit update destroy]
+  before_action :set_category, except: %i[index]
   before_action :set_task, only: %i[show edit update destroy]
   def index
     @tasks = Task.all
