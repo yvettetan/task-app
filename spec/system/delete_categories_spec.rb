63d7bb4c96cb4105100f1b9dd145955a513293d3
yvettetan <yvettetan97@gmail.com>
2021-08-06 19:51:37 +0800

    Updates test for deleting categories

--- spec/system/delete_categories_spec.rb
+++ spec/system/delete_categories_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Deleting Categories', type: :system, driver: :selenium_chrome, js: true do
   describe 'Deleting a category' do
     it 'successfully deletes a category' do
-      category = Category.create!(title: 'Title', description: 'description')
+      user = FactoryBot.create(:user)
+      login_as(user)
       visit categories_path
-      expect(page).to have_link 'Delete'
-      visit category_path(id: category.id)
+      category = Category.create!(title: 'Title', description: 'description', user_id: user.id)
+      visit category_path(category.id)
       click_on 'Delete'
       expect(page.driver.browser.switch_to.alert.text).to eq "Are you sure you want to delete #{category.title}?"
       page.driver.browser.switch_to.alert.accept
