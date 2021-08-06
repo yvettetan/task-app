38db58b3032b27251d94dc4eb372961ffb5f695d
yvettetan <yvettetan97@gmail.com>
2021-08-06 20:26:56 +0800

    Updates test for deleting tasks

--- spec/system/delete_tasks_spec.rb
+++ spec/system/delete_tasks_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting Tasks', type: :system, driver: :selenium_chrome, js: true do
-  describe 'Deleting a category' do
+  describe 'Deleting a task' do
     it 'successfully deletes a task' do
-      category = Category.new(title: 'Category Title', description: 'category description')
+      user = FactoryBot.create(:user)
+      login_as(user)
+      category = Category.new(title: 'Category Title', description: 'category description', user_id: user.id)
       task = category.tasks.build(name: 'task', description: 'description of task', due_at: Date.today.to_s,
-                                  category_id: category.id)
+                                  category_id: category.id, user_id: user.id)
       category.save
       visit category_task_path(category, task)
       click_on 'Delete'
