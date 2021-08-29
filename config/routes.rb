8e1fcd940423cc04d932877328a0dd2c65baa49c
yvettetan <yvettetan97@gmail.com>
2021-08-29 17:19:36 +0800

    Changes devise route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', sign_out: 'logout'}
   root 'tasks#index'
   resources :categories do
     resources :tasks, except: :index
