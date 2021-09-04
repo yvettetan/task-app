a0a5e18d7cf1c27225ada3f9d14599c46ae9a9d1
yvettetan <yvettetan97@gmail.com>
2021-09-04 10:54:53 +0800

    Adds database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,4 +29,24 @@ RSpec.configure do |config|
   config.after :each do
     Warden.test_reset!
   end
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
