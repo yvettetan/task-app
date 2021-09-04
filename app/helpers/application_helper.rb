78534a1e1bd8addd0be02d1137c8fd4632affe1c
yvettetan <yvettetan97@gmail.com>
2021-09-04 22:17:15 +0800

    Updates header style and creates helper for active navlink

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,9 @@ module ApplicationHelper
   def upcoming(tasks)
     tasks.where(['due_at > ?', Date.today]).order(due_at: :asc)
   end
+
+  def current_class?(test_path)
+    return 'active' if request.path == test_path
+    ''
+  end
 end
