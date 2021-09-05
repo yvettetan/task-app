adeaa25b248e414eb9e62056c6e332d8fc1a2a45
yvettetan <yvettetan97@gmail.com>
2021-09-05 23:30:22 +0800

    Adds completed attribute to task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_115434) do
+ActiveRecord::Schema.define(version: 2021_09_04_153203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_08_05_115434) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "due_at"
     t.integer "user_id"
+    t.boolean "completed", default: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
