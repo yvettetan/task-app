1fce1547edc2a091f8d538bc885b0530025f0d9b
yvettetan <yvettetan97@gmail.com>
2021-09-29 20:55:49 +0800

    Configures action mailer in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,5 +119,18 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Devise config
-  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'take-note36.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: ENV["MAILER_EMAIL"]}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'smtp.gmail.com',
+  user_name:            ENV["MAILER_USERNAME"],
+  password:             ENV["MAILER_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true,
 end
