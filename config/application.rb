6631190b00682b40d92ffaab4a90ee37160cc7da
yvettetan <yvettetan97@gmail.com>
2021-09-30 11:15:24 +0800

    Configures Dotenv

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module JournalApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -18,5 +17,8 @@ module JournalApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    if ['development', 'test'].include? ENV['RAILS_ENV']
+      Dotenv::Railtie.load
+    end
   end
 end
